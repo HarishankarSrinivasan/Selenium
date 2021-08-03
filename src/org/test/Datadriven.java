de54e53837e1ac1f8cfcda5a94a5f22b6b0bac8e
HarishankarSrinivasan <harishankar261991@gmail.com>
2021-08-03 14:51:34 +0530

    Project1

--- src/org/test/Datadriven.java
+++ src/org/test/Datadriven.java
@@ -4,4 +4,8 @@ public class Datadriven {
 	public static void username() {
 	System.out.println("Harishankar");	
 	 }
+	public static void main(String[] args) {
+		Datadriven n=new Datadriven();
+		n.username();
+	}
 }
